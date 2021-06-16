8331b36a144124f13c6f383922d0c17a46fe61c2
Randall Bohn <rsbohn@familysearch.org>
2021-06-16 06:32:40 -0600

    Implement VerticalBar
    Added VerticalBar implementation.
    TODO: HorizontalBar.

--- dexter_slider.py
+++ dexter_slider.py
@@ -41,6 +41,34 @@ import terminalio
 __version__ = "0.0.0-auto.0"
 __repo__ = "https://github.com/rsbohn/Dexter_CircuitPython_Slider.git"
 
+class BaseBar():
+    def __init__(self, limits=(0,100), value=50):
+        self.limits = limits
+        self.value = value
+
+    def scale(self, new_value):
+        """normalize new_value to a float [0.0:1.0)"""
+        return (new_value-self.limits[0])/(self.limits[1]-self.limits[0])
+
+class VerticalBar(BaseBar):
+    def __init__(self, width, height, limits, value, fill):
+        super().__init__(limits, value)
+        self.width = width
+        self.height = height
+        self.fill = fill
+        self.rect = self._Rect()
+
+    def _Rect(self):
+        scaled_height = int(self.scale(self.value)*self.height)
+        scaled_height = max(3,scaled_height)
+        return Rect(1, self.height-scaled_height+1, self.width-2, scaled_height-2, fill=self.fill)
+
+    def update(self, point):
+        a = (self.height-point[1])/self.height
+        self.value = (self.limits[1]-self.limits[0]) * a + self.limits[0]
+        self.rect = self._Rect()
+        return self.value
+
 class Slider(Widget, Control):
     def __init__(self, x, y, width, height, name, limits=(0,100), value=50, **kwargs):
         super().__init__(x=x,y=y,width=width,height=height, **kwargs)
@@ -54,8 +82,11 @@ class Slider(Widget, Control):
 
         self.frame = Rect(0,0,width,height,outline=self.frame_color)
         self.append(self.frame)
-        self.bar = self._make_bar(value)
-        self.append(self.bar)
+        if width >= height:
+            self.bar = HorizontalBar(width, height, limits=limits, value=value, fill=self.bar_color)
+        else:
+            self.bar = VerticalBar(width, height, limits=limits, value=value, fill=self.bar_color)
+        self.append(self.bar.rect)
         self.title = Label(terminalio.FONT, text=f"{self.name}:{self.value}", color=self.frame_color)
         self.title.anchor_point = (0,1/2)
         self.title.anchored_position = (8, height-12)
@@ -63,30 +94,9 @@ class Slider(Widget, Control):
     def contains(self, point):
         return super().contains(
                 (point[0]-self.x, point[1]-self.y))
-    def _make_bar(self, value):
-        vmin = self.limits[0]
-        vmax = self.limits[1]
-        width = int(self.width * (value - vmin) / (vmax - vmin))
-        return Rect(1,1, width-2, self.height-2, fill=self.bar_color)
-    def _scale(self, value):
-        vmin = self.limits[0]
-        vmax = self.limits[1]
-        new_value = int(vmin + (vmax-vmin) * (value - self.x) / self.width)
-        limited = False
-        if new_value > vmax:
-            new_value = vmax
-            limited = True
-        if new_value <= vmin:
-            new_value = vmin
-            limited = True
-        return new_value, limited
-    def set_value(self, value):
-        new_value, limited = self._scale(value)
-        self.value = new_value
-        self.remove(self.bar)
-        self.bar = self._make_bar(new_value)
-        if limited:
-            self.bar.fill = self.error_color
-        self.insert(1, self.bar)
-        self.title.text = f"{self.name}:{self.value}"
-        return self.value
+    def selected(self, point):
+        local_point = (point[0]-self.x, point[1]-self.y)
+        self.remove(self.bar.rect)
+        self.value = self.bar.update(local_point)
+        self.insert(1,self.bar.rect)
+        self.title.text = f"{self.name}:{int(self.value)}"
